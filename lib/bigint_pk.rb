0c5f8a88878b218787d2536692a9a7481eded19f
Guillaume Malette <guillaume@shopify.com>
2016-08-03 15:48:06 -0400

    Remove deprecations using prepend

--- lib/bigint_pk.rb
+++ lib/bigint_pk.rb
@@ -35,26 +35,26 @@ module BigintPk
     end
   end
 
+  module DefaultBigintForeignKeyReferences
+    def references(*args)
+      options = args.extract_options!
+      options.reverse_merge! limit: 8
+      # Limit shouldn't affect "#{col}_type" column in polymorphic reference.
+      # But don't change value if it isn't simple 'true'.
+      # Examples:
+      #   t.references :subject, null: false, polymorphic: true ==> t.integer :subject_id, limit: 8, null: false
+      #                                                             t.string  :subject_type, null: false
+      #   t.references :subject, polymorphic: { limit: 120 }    ==> t.integer :subject_id, limit: 8
+      #                                                             t.string  :subject_type, limit: 120
+      options[:polymorphic] = options.except(:polymorphic, :limit) if options[:polymorphic] == true
+      super( *args, options )
+    end
+  end
+
   def self.install_foreign_key_patches!
     [   ActiveRecord::ConnectionAdapters::TableDefinition,
         ActiveRecord::ConnectionAdapters::Table].each do |abstract_table_type|
-
-      abstract_table_type.class_eval do
-        def references_with_default_bigint_fk *args
-          options = args.extract_options!
-          options.reverse_merge! limit: 8
-          # Limit shouldn't affect "#{col}_type" column in polymorphic reference.
-          # But don't change value if it isn't simple 'true'.
-          # Examples:
-          #   t.references :subject, null: false, polymorphic: true ==> t.integer :subject_id, limit: 8, null: false
-          #                                                             t.string  :subject_type, null: false
-          #   t.references :subject, polymorphic: { limit: 120 }    ==> t.integer :subject_id, limit: 8
-          #                                                             t.string  :subject_type, limit: 120
-          options[:polymorphic] = options.except(:polymorphic, :limit) if options[:polymorphic] == true 
-          references_without_default_bigint_fk( *args, options )
-        end
-        alias_method_chain :references, :default_bigint_fk
-      end
+      abstract_table_type.prepend(DefaultBigintForeignKeyReferences)
     end
   end
 end
