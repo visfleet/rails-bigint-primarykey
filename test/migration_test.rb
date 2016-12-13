dd50c2d06b5d9180edc465f038e9b134c35e4969
Rafael Mendonça França <rafael.franca@shopify.com>
2016-12-12 21:38:10 -0500

    Write unit tests for the migrations patches
    The tests we had in this gem was using an entire rails application when
    it can be using the connection object to test the features. Now we are
    doing proper unit tests.

---
+++ test/migration_test.rb
@@ -0,0 +1,29 @@
+require 'minitest/autorun'
+require 'bigint_pk'
+require 'byebug'
+require 'active_record'
+
+BigintPk.enabled = true
+
+class MigrationTest < Minitest::Test
+  def setup
+    super
+    ActiveRecord::Base.establish_connection(adapter: "mysql2", database: "bigint_test")
+    ActiveRecord::Base.logger = Logger.new(STDOUT)
+  end
+
+  def teardown
+    super
+    ActiveRecord::Base.connection.drop_table("foo") rescue nil
+  end
+
+  def test_creating_a_table_use_bigint_as_primary_key
+    connection = ActiveRecord::Base.connection
+    connection.create_table('foo')
+    columns = connection.columns(:foo)
+    assert_equal ['id'], columns.map(&:name)
+    assert_equal [:integer], columns.map(&:type)
+    assert_equal ['bigint(20)'], columns.map(&:sql_type)
+    assert_equal [8], columns.map(&:limit)
+  end
+end
